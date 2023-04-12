689140876c1b7195b1b35600ca8a84558fadf498
harshsimform <harsh.v@simformsolutions.com>
2023-04-12 16:07:43 +0530

    webpack with css-loader

---
+++ src/App.js
@@ -0,0 +1,4 @@
+import { sayHello } from "./component";
+import "./style.css";
+sayHello();
+console.log("harsh patel");
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+module.exports = {
+  mode: "production",
+  entry: "./src/App.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 6455,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
